85ef4b04ee4e34007457996a0557704e3ac5d319
Ryota Konno <r.konno@nowall.co.jp>
2016-12-28 02:37:33 +0900

    [modify] ユーザからの入力を受け付けるコンポーネント
    ①getInitialStateメソッドにてステートを初期化
    ②expressionというステートの値を空の文字列に初期化
    ③ユーザ入力を検知すると呼ばれるreCalcValueメソッドを定義
    ④押されたキーがEnterのときだけ以下の処理を実行
    ⑤this.setStateメソッドによってステート値を更新
    ⑥入力された値（文字列）をexpressionステートにセット
    ⑦inputの中でキーが押された場合，reCalcValueを呼ぶように設定
    ⑧h2要素にexpressionステートの評価結果を書き込む

--- src/react_script.js
+++ src/react_script.js
@@ -1,26 +1,45 @@
 var HelloWorld = React.createClass({
-   render: function() {
-       return React.DOM.h2(null, 'Hello, ' + this.props.name + 'World!');
-   }
+    render: function () {
+        return React.DOM.h2(null, 'Hello, ' + this.props.name + 'World!');
+    }
 });
 
 var Evaluator = React.createClass({
-   render: function() {
-       return React.DOM.h2(null, eval(this.props.text));
-   }
+    getInitialState: function() {
+      return {
+          expression: ''
+      }  ;
+    },
+    reCalcValue: function(e) {
+        if (e.key === 'Enter')
+            this.setState({
+                expression: e.target.value
+            });
+    },
+    render: function() {
+        return React.DOM.div(
+            null,
+            React.DOM.input({
+                type: 'text',
+                onKeyPress: this.reCalcValue
+            }),
+            React.DOM.h2(null, eval(this.state.expression))
+        );
+    }
 });
 
 var MultiElement = React.createClass({
-   render: function () {
-       return React.DOM.div(null,
-         React.DOM.h2(null, 'First'),
-         React.DOM.h2(null, 'Second'),
-         React.DOM.h2(null, 'Third')
-       );
-   }
+    render: function () {
+        return React.DOM.div(
+            null,
+            React.DOM.h2(null, 'First'),
+            React.DOM.h2(null, 'Second'),
+            React.DOM.h2(null, 'Third')
+        );
+    }
 });
 
 ReactDOM.render(
-  React.createElement(MultiElement),
+    React.createElement(Evaluator),
     document.getElementById('content')
 );
