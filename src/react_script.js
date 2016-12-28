da7200f4def5689ccc4c1f31cc21eac404a6894f
Ryota Konno <r.konno@nowall.co.jp>
2016-12-29 00:37:28 +0900

    条件（三項演算子）を使う

--- src/react_script.js
+++ src/react_script.js
@@ -7,15 +7,15 @@ var HelloWorld = React.createClass({
 });
 
 var MultiElements = React.createClass({
-   render: function () {
-       return (
-           <div>
-               <h2>First Element</h2>
-               <h2>Second Element</h2>
-               <h2>Third Element</h2>
-           </div>
-       )
-   }
+    render: function () {
+        return (
+            <div>
+                <h2>First Element</h2>
+                <h2>Second Element</h2>
+                <h2>Third Element</h2>
+            </div>
+        )
+    }
 });
 
 var Evaluator = React.createClass({
@@ -31,20 +31,30 @@ var Evaluator = React.createClass({
             });
         }
     },
-   render: function () {
-       return (
-           <div>
-               <input
-                   type='text'
-                   onKeyPress={this.reCalcValue}
-               />
-               <h2>{eval(this.state.expression)}</h2>
-           </div>
-       )
-   }
+    render: function () {
+        return (
+            <div>
+                <input
+                    type='text'
+                    onKeyPress={this.reCalcValue}
+                />
+                <h2>{eval(this.state.expression)}</h2>
+            </div>
+        )
+    }
+});
+
+var RandomColor = React.createClass({
+    render: function () {
+        return (
+            <h2 style={{color: this.props.value >= 0 ? 'green' : 'red'}}>
+                the number is {this.props.value}
+            </h2>
+        )
+    }
 });
 
 ReactDOM.render(
-    <Evaluator/>,
+    <RandomColor value={Math.floor(Math.random() * 20) - 10}/>,
     document.getElementById('content')
 );
