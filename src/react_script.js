5fa864f8f1b613293ab604698ed47112cad04dc6
Ryota Konno <r.konno@nowall.co.jp>
2016-12-29 01:53:39 +0900

    [modify] onPressKeyを使ったステート変更

--- src/react_script.js
+++ src/react_script.js
@@ -1,7 +1,20 @@
 var TextInput = React.createClass({
+    getInitialState: function () {
+        return {dispText: '入力欄'};
+    },
+    handleInput: function (event) {
+        this.setState({dispText: event.target.value});
+        console.log(event.target.value);
+    },
     render: function () {
         return (
-            <div>JSX Input: <input type='text' value='入力欄…'/></div>
+            <div>JSX Input:
+                <input
+                    type='text'
+                    value={this.state.dispText}
+                    onChange={this.handleInput}
+                />
+            </div>
         )
     }
 });
