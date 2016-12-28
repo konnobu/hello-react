42e086f07bce99b7719be94c4693a03a09274836
Ryota Konno <r.konno@nowall.co.jp>
2016-12-29 00:59:04 +0900

    [modify] カスタムコンポーネントでスプレッド属性を利用

--- src/react_script.js
+++ src/react_script.js
@@ -1,64 +1,24 @@
-var HelloWorld = React.createClass({
+var ColoredButton = React.createClass({
     render: function () {
         return (
-            <h2>Hello, {this.props.name} World!</h2>
-        )
-    }
-});
-
-var MultiElements = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h2>First Element</h2>
-                <h2>Second Element</h2>
-                <h2>Third Element</h2>
-            </div>
-        )
-    }
-});
-
-var Evaluator = React.createClass({
-    getInitialState: function () {
-        return {
-            expression: ''
-        };
-    },
-    reCalcValue: function (e) {
-        if (e.key === 'Enter') {
-            this.setState({
-                expression: e.target.value
-            });
-        }
-    },
-    render: function () {
-        return (
-            <div>
-                <input
-                    type='text'
-                    onKeyPress={this.reCalcValue}
-                />
-                <h2>{eval(this.state.expression)}</h2>
-            </div>
-        )
-    }
-});
-
-var RandomColor = React.createClass({
-    render: function () {
-        return (
-            <h2 style={{color: this.props.value >= 0 ? 'green' : 'red'}}>
-                the number is {this.props.value}
-            </h2>
+            <button {...this.props}>
+                {this.props.children}
+            </button>
         )
     }
 });
 
 ReactDOM.render(
-    <div>
-        <p><input type="checkbox" checked/>JSX Check!</p>
-        <p><input type="checkbox" checked="{true}"/>JSX Check2!</p>
-        <p><input type="checkbox" checked="{false}"/>JSX Check3!</p>
-    </div>,
+    <ColoredButton
+        style={{
+            color: '#F26964',
+            backgroundColor: '#1253A4',
+            fontFamily: 'serif',
+            fontsize: 22
+        }}
+        type='button'
+        disable={false}>
+        Click Me!
+    </ColoredButton>,
     document.getElementById('content')
 );
