5b37b9033e70b7f1feb6569899b9999d35184d53
Ryota Konno <r.konno@nowall.co.jp>
2016-12-28 11:44:33 +0900

    [modify] コンポーネントのライフサイクルを確認

--- src/react_script.js
+++ src/react_script.js
@@ -5,6 +5,28 @@ var HelloWorld = React.createClass({
 });
 
 var Evaluator = React.createClass({
+    componentWillMount: function () {
+      console.log('componentWillMount:');
+    },
+    componentDidMount: function () {
+      console.log('componentDidMount');
+    },
+    componentWillReceiveProps: function (nextProps) {
+      console.log('componentWillReceiveProps:', nextProps);
+    },
+    shouldComponentUpdate: function (nextProps, nextState) {
+      console.log('shouldComponentUpdate:', nextProps, nextState);
+      return true;
+    },
+    componentWillUpdate: function (nextProps, nextState) {
+      console.log('componentWillUpdate:', nextProps, nextState);
+    },
+    componentDidUpdate: function (nextProps, nextState) {
+      console.log('componentDidUpdate:', nextProps, nextState);
+    },
+    componentWillUnmount: function () {
+      console.log('componentWillUnmount:');
+    },
     getInitialState: function() {
       return {
           expression: ''
