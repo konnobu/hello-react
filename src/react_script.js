eb7cea41d1aaf6329729d783e37e2e69097e393e
Ryota Konno <r.konno@nowall.co.jp>
2016-12-29 00:32:20 +0900

    [modify] JSXでEvaluator

--- src/react_script.js
+++ src/react_script.js
@@ -1,4 +1,50 @@
+var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <h2>Hello, {this.props.name} World!</h2>
+        )
+    }
+});
+
+var MultiElements = React.createClass({
+   render: function () {
+       return (
+           <div>
+               <h2>First Element</h2>
+               <h2>Second Element</h2>
+               <h2>Third Element</h2>
+           </div>
+       )
+   }
+});
+
+var Evaluator = React.createClass({
+    getInitialState: function () {
+        return {
+            expression: ''
+        };
+    },
+    reCalcValue: function (e) {
+        if (e.key === 'Enter') {
+            this.setState({
+                expression: e.target.value
+            });
+        }
+    },
+   render: function () {
+       return (
+           <div>
+               <input
+                   type='text'
+                   onKeyPress={this.reCalcValue}
+               />
+               <h2>{eval(this.state.expression)}</h2>
+           </div>
+       )
+   }
+});
+
 ReactDOM.render(
-    <h2>Hello, React World!!</h2>,
+    <Evaluator/>,
     document.getElementById('content')
 );
